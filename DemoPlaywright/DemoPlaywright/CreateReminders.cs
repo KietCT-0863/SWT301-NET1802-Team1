7e58e7eff7e3a9639d1c0ff180d03ae359460bcb
minhtr-coder <trichmse172870@fpt.edu.vn>
2025-03-22 21:38:11 +0700

    Create reminders

--- DemoPlaywright/DemoPlaywright/CreateReminders.cs
+++ DemoPlaywright/DemoPlaywright/CreateReminders.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 
 namespace DemoPlaywright
 {
-    public class LoginTest
+    public class CreateReminders
     {
         private IPlaywright playwright;
         private IBrowser browser;
@@ -42,16 +42,16 @@ namespace DemoPlaywright
         {
             List<List<string>> listAccountTest = new();
 
-            listAccountTest.Add(new List<string> { "vip1", "vip123" });
-            listAccountTest.Add(new List<string> { "kiet", "kiet123" });
+            // listAccountTest.Add(new List<string> { "vip1", "vip123" });
+            // listAccountTest.Add(new List<string> { "kiet", "kiet123" });
             listAccountTest.Add(new List<string> { "swt", "swt123" });
-            listAccountTest.Add(new List<string> { "member1", "member123" });
+            // listAccountTest.Add(new List<string> { "member1", "member123" });
 
             return listAccountTest;
         }
 
         [TestCaseSource(nameof(initData))]
-        public async Task TestLoginWithAvailableMemberAccount(List<string> account)
+        public async Task TestCreateReminders(List<string> account)
         {
             await page.GotoAsync("https://pregnancy-growth-tracking.vercel.app/login");
 
@@ -70,6 +70,31 @@ namespace DemoPlaywright
             // Assert using NUnit
             string? messageText = await toastMessage.TextContentAsync();
             Assert.That(messageText, Is.EqualTo("Đăng nhập thành công!"));
+
+            await Task.Delay(3000);
+
+            await page.ClickAsync("//button[normalize-space()='OK']");
+
+            await page.ClickAsync("a[href='/member/calendar']");
+
+            await page.ClickAsync(".new-event-button");
+
+            await page.FillAsync("input[placeholder='Tiêu đề']", "Sức khỏe");
+
+            // Lấy ngày và giờ hiện tại
+            DateTime now = DateTime.Now;
+            string currentDate = now.ToString("2025-03-22");  
+            string currentTime = now.ToString("16:45");       
+
+            // Điền ngày và giờ hiện tại vào form
+            await page.FillAsync("input[type='date']", currentDate);
+            await page.FillAsync("input[type='time']", currentTime);    
+
+            await page.SelectOptionAsync("div[class='form-group'] select", new SelectOptionValue { Value = "Uống thuốc" });
+
+            await page.FillAsync("textarea[placeholder='Thông báo (không bắt buộc)']", "Bạn hãy nhớ uống thuốc đúng giờ nhé.");
+
+            await page.ClickAsync("button[class='save-btn']");
 
             await Task.Delay(3000);
         }
