534ad98d96ff189280cdd6815ec648d139315180
KietCT-0863 <kietvipro123456789@gmail.com>
2025-03-22 15:19:17 +0700

    update LoginTest to click login button using JavaScript

--- DemoPlaywright/DemoPlaywright/LoginTest.cs
+++ DemoPlaywright/DemoPlaywright/LoginTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.Playwright;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.Runtime.InteropServices;
 
 namespace DemoPlaywright
 {
@@ -24,7 +25,7 @@ namespace DemoPlaywright
                     //"--inprivate"        // Chế độ ẩn danh của Edge
                 }
             });
-            
+
             // Tạo context mới
             var context = await browser.NewContextAsync();
             page = await context.NewPageAsync();
@@ -43,54 +44,36 @@ namespace DemoPlaywright
             List<List<string>> listAccountTest = new();
 
             listAccountTest.Add(new List<string> { "vip1", "vip123" });
+            listAccountTest.Add(new List<string> { "kiet", "kiet123" });
             listAccountTest.Add(new List<string> { "swt", "swt123" });
             listAccountTest.Add(new List<string> { "member1", "member123" });
-            listAccountTest.Add(new List<string> { "kiet", "kiet123" });
 
             return listAccountTest;
         }
 
         [TestCaseSource(nameof(initData))]
-        public async Task TestLoginWithAvailableAccount(List<string> account)
+        public async Task TestLoginWithAvailableMemberAccount(List<string> account)
         {
             await page.GotoAsync("https://pregnancy-growth-tracking.vercel.app/login");
-            
+
             await page.FillAsync("#usernameOrEmail", account[0]);
             await page.FillAsync("#password", account[1]);
-            await page.ClickAsync("button:has-text('Đăng nhập')");
 
-            // Đợi cho toast message xuất hiện và kiểm tra nội dung
-            IElementHandle? toastMessage = await page.WaitForSelectorAsync(".Toastify__toast-body > div:nth-child(2)");
-            string? messageText = await toastMessage.TextContentAsync();
-            
+            // Click nút Login bằng JavaScript
+            await page.EvaluateAsync(@"() => {
+                const button = document.querySelector('button[type=""submit""]');
+                if (button) button.click();
+                }");
+
+            // Kiểm tra toast message bằng XPath
+            var toastMessage = await page.WaitForSelectorAsync("//div[contains(text(),'Đăng nhập thành công!')]");
+
             // Assert using NUnit
+            string? messageText = await toastMessage.TextContentAsync();
             Assert.That(messageText, Is.EqualTo("Đăng nhập thành công!"));
 
             await Task.Delay(3000);
-
-            // Click vào "Lịch Trình Thăm Khám"
-            await page.ClickAsync("a[href='/member/calendar']");
-            // Click vào nút "Thêm sự kiện"
-            await page.ClickAsync(".add-event-btn");
-            // Điền thông tin sự kiện
-            await page.FillAsync("input[placeholder='Tiêu đề']", "Sức Khỏe");
-            await page.FillAsync("input[type='date']", "2025-03-22");
-            await page.FillAsync("input[type='time']", "11:00");
-            // Chọn danh mục từ 
-            await page.SelectOptionAsync("div.modal-content form select", new SelectOptionValue { Value = "Uống thuốc" });
-            // Điền thông báo
-            await page.FillAsync("textarea[placeholder='Thông báo']", "Bạn hãy nhớ uống thuốc đúng giờ nhé.");
-            
-            await page.ClickAsync("button[type='submit']");
-
-            await Task.Delay(5000);
-
-            toastMessage = await page.WaitForSelectorAsync(".Toastify__toast-body > div:nth-child(2)");
-            messageText = await toastMessage.TextContentAsync();
-
-            Assert.That(messageText, Is.EqualTo("Lưu thành công!"));
-
-            await Task.Delay(5000); 
         }
+
     }
 }
